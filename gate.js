8a1b99f9721d0374cd897d0cc0a331218eb590cc
tenagusami <tenagusami_mail@yahoo.co.jp>
2017-05-30 17:33:03 +0900

    xor and activation functions are implemented.

--- gate.js
+++ gate.js
@@ -4,17 +4,25 @@ module.exports = (()=>{
   const U=require('./utility.js');
   const R=require('ramda');
   const P=require('./perceptron.js');
+
+  const p=P.perceptron;
   
-  const andGate=P.perceptron([0.5,0.5],-0.7,0);
+  const andGate=p([0.5,0.5],-0.7,0);
 
-  const orGate=P.perceptron([0.5,0.5],-0.2,0);
+  const orGate=p([0.5,0.5],-0.2,0);
 
-  const nandGate=P.perceptron([-0.5,-0.5],0.7,0);
+  const nandGate=p([-0.5,-0.5],0.7,0);
+
+  const xorGate=(inputArray)=>{
+    return andGate([nandGate([inputArray[0],inputArray[1]]),
+		   orGate([inputArray[0],inputArray[1]])]);
+  };
   
   return {
     andGate:andGate,
     nandGate:nandGate,
-    orGate:orGate
+    orGate:orGate,
+    xorGate: xorGate
   };
   
 })();
--- perceptron.js
+++ perceptron.js
@@ -4,10 +4,27 @@ module.exports = (()=>{
   const U=require('./utility.js');
   const R=require('ramda');
   const C=require('./coordinate.js');
+  const math=require('mathjs');
 
+  const stepFunction=(x)=>{
+    return x>0?1:0;
+  };
+
+  const sigmoidFunction=(x)=>{
+    return 1/(1+math.exp(-x));
+  };
+
+  const rectiFiedLinearUnitFunction=(x)=>{
+    return math.max(0,x);
+  };
+  
+  //const activationFunction=stepFunction;
+  const activationFunction=sigmoidFunction;
+  
+  
   const perceptron=R.curry((weight,bias,threshold)=>{
     return (array)=>{
-      return C.innerProduct(array,weight)+bias>threshold;
+      return activationFunction(C.innerProduct(array,weight)+bias-threshold);
     };
   });
 
